b182a3125a95578f733d07e401dcd465139af6dd
Omid <omidkarimi54@gmail.com>
2023-08-13 14:58:15 +0430

    first OOP version of greeting // it has a revisit check that is not compelete yet!

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+class Greetings():
+    def __init__(self , name , lname) -> None:
+        self.name = name
+        self.lname = lname
+        if self.revisit_check() == True:
+            self.revisit()
+        else:
+            self.visit()
+
+    def revisit_check(self):
+        return False
+    
+    def visit(self):
+        print('Nice to meet you! Welcome to my first git oriented programming!')
+
+    def revisit(self):
+        print("Welcome back {}! You've been here {} Tiems!".format(self.name , self.visits_Num()))
+
+    def visits_Num(self):
+        return 1
+    
 name = input('Whats your name ?')
-print('Hi {}! This is my first git oriented code :)'.format(name))
+lname = input('Whats your last name ?')
+name_greet = Greetings(name , lname)
