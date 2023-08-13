1c95e6f5f8c03ae09380d126efb6164fe6d3f22e
Omid <omidkarimi54@gmail.com>
2023-08-13 15:34:17 +0430

    ignore file created

--- main.py
+++ main.py
@@ -8,16 +8,37 @@ class Greetings():
             self.visit()
 
     def revisit_check(self):
+        name_check = self.name + ' ' + self.lname
+        with open('names.txt', 'r') as file:
+            for line in file:
+                if name_check in line:
+                    self.num = int(line[-2])
+                    return True
         return False
     
     def visit(self):
         print('Nice to meet you! Welcome to my first git oriented programming!')
+        line = self.name + ' ' + self.lname + ' 1\n' 
+        with open('names.txt', 'a') as file:
+            file.write(line)
 
     def revisit(self):
+        self.num += 1
         print("Welcome back {}! You've been here {} Tiems!".format(self.name , self.visits_Num()))
 
+        with open('names.txt', 'r') as file:
+            lines = file.readlines()
+
+        for i in range(len(lines)):
+            if self.name + ' ' + self.lname in lines[i]:
+                lines[i] = self.name + ' ' + self.lname + ' ' + str(self.num) + '\n'
+
+        with open('names.txt', 'w') as file:
+            file.writelines(lines)
+
+
     def visits_Num(self):
-        return 1
+        return self.num
     
 name = input('Whats your name ?')
 lname = input('Whats your last name ?')
